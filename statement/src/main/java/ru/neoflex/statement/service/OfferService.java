19397dc7d06396783bd855d30f975971678aeeb8
HedgeHogSE <sasha.ezhak2006@gmail.com>
2026-04-11 15:39:32 +0400

    added tests, documentation via wagger, and logging. removed prescoring from the calculator module.

--- statement/src/main/java/ru/neoflex/statement/service/OfferService.java
+++ statement/src/main/java/ru/neoflex/statement/service/OfferService.java
@@ -10,16 +10,19 @@ import ru.neoflex.statement.service.command.LoanOfferCommand;
 import ru.neoflex.statement.service.command.LoanStatementRequestCommand;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OfferService {
 
     private final RestClient restClient;
 
     public List<LoanOfferDto> getLoanOffers(LoanStatementRequestCommand command) {
 
-        return restClient
+        log.info("Sending deal/statement request for amount {}, term {}", command.getAmount(), command.getTerm());
+        List<LoanOfferDto> response = restClient
                 .post()
                 .uri("/deal/statement")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -27,10 +30,14 @@ public class OfferService {
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
                 });
+        log.info("Received {} loan offers from deal module", response == null ? 0 : response.size());
+        return response;
     }
 
     public void selectOffer(LoanOfferCommand command) {
 
+        log.info("Sending deal/offer/select request for statementId: {}", command.getStatementId());
+        
         restClient
                 .post()
                 .uri("/deal/offer/select")
@@ -39,6 +46,8 @@ public class OfferService {
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
                 });
+                
+        log.info("Successfully selected offer for statementId: {}", command.getStatementId());
     }
 
 
